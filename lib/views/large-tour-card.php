9fc1e48b256eb23d228bdfa442fe288a78bae053
Kate <hello@kateamann.com>
2018-12-10 18:49:12 +0000

    price changes

--- lib/views/large-tour-card.php
+++ lib/views/large-tour-card.php
@@ -3,7 +3,13 @@
 	<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 			<?php the_title(); ?>
 			<span class="price">
-				ab <?php the_field('price'); ?> €
+				<?php 
+		        if(get_field('discount_price')) { ?>
+		            ab <span class="original"><?php the_field('price'); ?> €</span><span class="discount"><?php the_field('discount_price'); ?> €</span> <?php
+		        } 
+		        else { ?>
+		            ab <?php the_field('price'); ?> €<?php
+		        } ?>
 			</span>
 		</a>
 	</h3>
