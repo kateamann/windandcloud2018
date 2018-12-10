2707bd73bbb11a4a7bf323d56a1ace89c4fcbaa7
Kate <hello@kateamann.com>
2018-12-10 18:51:15 +0000

    Revert "price changes"
    This reverts commit 9fc1e48b256eb23d228bdfa442fe288a78bae053.

--- lib/views/large-tour-card.php
+++ lib/views/large-tour-card.php
@@ -3,13 +3,7 @@
 	<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 			<?php the_title(); ?>
 			<span class="price">
-				<?php 
-		        if(get_field('discount_price')) { ?>
-		            ab <span class="original"><?php the_field('price'); ?> €</span><span class="discount"><?php the_field('discount_price'); ?> €</span> <?php
-		        } 
-		        else { ?>
-		            ab <?php the_field('price'); ?> €<?php
-		        } ?>
+				ab <?php the_field('price'); ?> €
 			</span>
 		</a>
 	</h3>
--- lib/views/small-tour-card-loop.php
+++ lib/views/small-tour-card-loop.php
@@ -2,13 +2,7 @@
     <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
         <h4><?php the_title(); ?></h4>
         <?php the_post_thumbnail( 'featured-link' );?>
-        <div class="tour-overlay"><?php 
-            if(get_field('discount_price')) { ?>
-                ab <span class="original"><?php the_field('price'); ?> €</span><span class="discount"><?php the_field('discount_price'); ?> €</span> <?php
-            } 
-            else { ?>
-                ab <?php the_field('price'); ?> € <?php
-            } ?>
+        <div class="tour-overlay">ab <?php the_field('price'); ?> €
         </div>
     </a>
 </div>
